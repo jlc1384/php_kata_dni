c3e58cc9d4ea7ed2c428557cfbe098585f567cfa
Jesus Lago <jesus.lago@letsrebold.com>
2022-04-10 17:45:31 +0200

    Sixth test. Check the dni first character is a number X, Y or Z. Green cycle. Refactor Dni class code.

--- app/Dni/Integrations/Dni.php
+++ app/Dni/Integrations/Dni.php
@@ -12,18 +12,19 @@ class Dni
     public function __construct($dni)
     {
         $this->checkDniLength($dni);
-        //if(preg_match('/[0-9]/', substr($dni, -1))) {
         if(preg_match('/\d$/', $dni)) {
             throw new InvalidDniFormatException("Last Dni character can't be a number");
         }
-        //if(preg_match('(i|o|ñ|u)', strtolower($dni)) === 1) {
         if (preg_match('/[UIOÑ]$/u', $dni)) {
             throw new InvalidDniFormatException("Last Dni character can't be i,o,u,ñ");
         }
         if(!preg_match('/\d{7,7}.$/', $dni)) {
             throw new InvalidDniFormatException("Dni can't have characters in the middle of the string");
         }
-        throw new InvalidDniFormatException("Dni must start by a number X, Y or Z");
+        if(!preg_match('/^[XYZ0-9]/', $dni)) {
+            throw new InvalidDniFormatException("Dni must start by a number X, Y or Z");
+        }
+
     }
 
     /**
