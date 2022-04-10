63664e01724bdf50b3a7c120cb1d5d01bc0f9164
Jesus Lago <jesus.lago@letsrebold.com>
2022-04-10 10:01:53 +0200

    Third test. Check the last dni character can't be a number. Refactor. Class Dni. Added constant with value equals to 9. We will use it to check if dni length is valid.

--- app/Dni/Integrations/Dni.php
+++ app/Dni/Integrations/Dni.php
@@ -7,12 +7,14 @@ use App\Dni\Exceptions\InvalidDniFormatException;
 
 class Dni
 {
+    const DNI_LENGTH_TO_CHECK = 9;
+
     public function __construct($dni)
     {
-        if(strlen($dni) > 9) {
+        if(strlen($dni) > self::DNI_LENGTH_TO_CHECK) {
             throw new InvalidDniLengthException('Dni string is too long');
         }
-        if(strlen($dni) < 9) {
+        if(strlen($dni) < self::DNI_LENGTH_TO_CHECK) {
             throw new InvalidDniLengthException('Dni string is too short');
         }
         throw new InvalidDniFormatException("Last Dni character can't be a number");
