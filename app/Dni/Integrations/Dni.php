22f47f8f8d70784ab9bf3007bc29a70a71b3aafb
Jesus Lago <jesus.lago@letsrebold.com>
2022-04-10 17:46:38 +0200

    Sixth test. Check the dni first character is a number X, Y or Z. Green cycle. Refactor Dni class code. Extracted all string structure conditions to a private function.

--- app/Dni/Integrations/Dni.php
+++ app/Dni/Integrations/Dni.php
@@ -9,22 +9,14 @@ class Dni
 {
     const DNI_LENGTH_TO_CHECK = 9;
 
+    /**
+     * @throws InvalidDniFormatException
+     * @throws InvalidDniLengthException
+     */
     public function __construct($dni)
     {
         $this->checkDniLength($dni);
-        if(preg_match('/\d$/', $dni)) {
-            throw new InvalidDniFormatException("Last Dni character can't be a number");
-        }
-        if (preg_match('/[UIOÑ]$/u', $dni)) {
-            throw new InvalidDniFormatException("Last Dni character can't be i,o,u,ñ");
-        }
-        if(!preg_match('/\d{7,7}.$/', $dni)) {
-            throw new InvalidDniFormatException("Dni can't have characters in the middle of the string");
-        }
-        if(!preg_match('/^[XYZ0-9]/', $dni)) {
-            throw new InvalidDniFormatException("Dni must start by a number X, Y or Z");
-        }
-
+        $this->checkDniStringStructure($dni);
     }
 
     /**
@@ -35,6 +27,26 @@ class Dni
     {
         if (strlen($dni) !== self::DNI_LENGTH_TO_CHECK) {
             throw new InvalidDniLengthException('Dni string is not valid. It must contain exactly 9 characters');
+        }
+    }
+
+    /**
+     * @param $dni
+     * @throws InvalidDniFormatException
+     */
+    private function checkDniStringStructure($dni): void
+    {
+        if (preg_match('/\d$/', $dni)) {
+            throw new InvalidDniFormatException("Last Dni character can't be a number");
+        }
+        if (preg_match('/[UIOÑ]$/u', $dni)) {
+            throw new InvalidDniFormatException("Last Dni character can't be i,o,u,ñ");
+        }
+        if (!preg_match('/\d{7,7}.$/', $dni)) {
+            throw new InvalidDniFormatException("Dni can't have characters in the middle of the string");
+        }
+        if (!preg_match('/^[XYZ0-9]/', $dni)) {
+            throw new InvalidDniFormatException("Dni must start by a number X, Y or Z");
         }
     }
 }
