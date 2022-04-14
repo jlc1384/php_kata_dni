bbda26baf4e81c1dae5489af240fdcf064443223
Jesus Lago <jesus.lago@letsrebold.com>
2022-04-14 17:24:40 +0200

    Eight test. Check the Dni class set the dni as valid, and we can access it for the specific Dni string 00000001R. Refactor. Added to equivalences variable the rest of equivalences of pairs reminder - letter

--- app/Dni/Integrations/Dni.php
+++ app/Dni/Integrations/Dni.php
@@ -25,9 +25,10 @@ class Dni
         $dniNumber = substr($dni, 0, - 1);
         $dniLetter = strtoupper(substr($dni, -1));
         $remainder = $dniNumber % 23;
-        $dniEquivalencesLetterRemainder = [
-          "0" => "T",
-          "1" => "R"
+        $dniEquivalencesLetterRemainder = ["0" => "T", "1" => "R", "2" => "W", "3" => "A", "4" => "G", "5" => "M",
+            "6" => "Y", "7" => "F", "8" => "P", "9" => "D", "10" => "X", "11" => "B", "12" => "N", "13" => "J",
+            "14" => "Z", "15" => "S", "16" => "Q", "17" => "V", "18" => "H", "19" => "L", "20" => "C", "21" => "K",
+            "22" => "E"
         ];
 
         if($dniLetter !== $dniEquivalencesLetterRemainder[$remainder]) {
