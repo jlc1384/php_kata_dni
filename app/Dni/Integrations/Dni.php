aacb39e552c73039f15494119919904543c4f86c
Jesus Lago <jesus.lago@letsrebold.com>
2022-04-14 17:59:01 +0200

    Eleventh test. Check the Dni class set the dni as valid, and we can access it for the specific Dni string Z0000000M. Refactor.

--- app/Dni/Integrations/Dni.php
+++ app/Dni/Integrations/Dni.php
@@ -23,11 +23,10 @@ class Dni
         $this->checkDniStringStructure($dni);
 
         $dniNumber = substr($dni, 0, - 1);
-        $dniNumber = str_replace("X", '0', $dniNumber);
-        $dniNumber = str_replace("Y", '1', $dniNumber);
-        $dniNumber = str_replace("Z", '2', $dniNumber);
+        $dniNumber = str_replace(["X", "Y", "Z"], ['0', '1', '2'], $dniNumber);
         $dniLetter = strtoupper(substr($dni, -1));
         $remainder = $dniNumber % 23;
+
         $dniEquivalencesLetterRemainder = ["0" => "T", "1" => "R", "2" => "W", "3" => "A", "4" => "G", "5" => "M",
             "6" => "Y", "7" => "F", "8" => "P", "9" => "D", "10" => "X", "11" => "B", "12" => "N", "13" => "J",
             "14" => "Z", "15" => "S", "16" => "Q", "17" => "V", "18" => "H", "19" => "L", "20" => "C", "21" => "K",
