ddcfc1fca01e9eca4de77e007ad78c43aafb5153
Jesus Lago <jesus.lago@letsrebold.com>
2022-05-24 09:08:18 +0200

    Red cycle. Start kata

--- tests/Tests/Feature/Katas/Dni/DniTest.php
+++ tests/Tests/Feature/Katas/Dni/DniTest.php
@@ -9,8 +9,7 @@ class DniTest  extends TestCase
 {
     public function test_dni_is_invalid_because_dni_string_is_too_long()
     {
-        $this->expectException(InvalidDniLengthException::class);
-        $this->expectExceptionMessage('Dni string is too long');
+        $this->expectException(DniStringLengthTooLongException::class);
         new Dni();
     }
 }
