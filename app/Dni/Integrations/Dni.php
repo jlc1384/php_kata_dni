446ca21ce7bc1add227a94731aae45eac8bae1db
Jesus Lago <jesus.lago@letsrebold.com>
2022-05-24 09:06:49 +0200

    Red cycle. Start kata

--- app/Dni/Integrations/Dni.php
+++ app/Dni/Integrations/Dni.php
@@ -2,12 +2,10 @@
 
 namespace App\Dni\Integrations;
 
-use App\Dni\Exceptions2\InvalidDniLengthException;
-
 class Dni
 {
     public function __construct()
     {
-        throw new InvalidDniLengthException('Dni string is too long');
+
     }
 }
